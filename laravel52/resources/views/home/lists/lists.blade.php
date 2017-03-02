d9a5099a30639a6542675e15f2482d662c739a39
xiaodu <644941384@qq.com>
2017-03-02 09:14:21 +0800

    11

--- laravel52/resources/views/home/lists/lists.blade.php
+++ laravel52/resources/views/home/lists/lists.blade.php
@@ -301,25 +301,25 @@
 
 
         <div id="foot" class="foot clear">
-            <p><a href="javascript:void(0)../help.aspx?ClientID=" class="c1">帮助</a><a  href="javascript:void(0)../client.aspx?ClientID=" class="c1">客户端</a><a href="javascript:void(0)../idear.aspx?ClientID=" class="c1">意见反馈</a></p>
+            <p><a href="" class="c1">帮助</a><a  href="" class="c1">客户端</a><a href="">意见反馈</a></p>
             <p><a href="javascript:void(0)tel:400-066-8882" style="margin-right:10px;" id="CustomService">400-066-8882</a>     http://aimovie.duzejun.cn</p>
             <p class="f10">Copyright2013- 2017爱电影版权所有. </p>
         </div>
-        <script>
-            var CityName=document.getElementById("defaultCityName").innerHTML = Utils.getCookie("CityName");
-            if (CityName == '北京市') {
-                document.getElementById("CustomService").innerHTML = '400-010-1515';
-                document.getElementById("CustomService").href = 'tel:400-010-1515';
-            }
-            else {
-                document.getElementById("CustomService").innerHTML = '400-066-8882';
-                document.getElementById("CustomService").href = 'tel:400-066-8882';
-            }
-            var ClientID = document.getElementById("ClientID").value;
-            if (ClientID == 'C100000175') {
-                document.getElementById("foot").style.display="none";
-            }
-        </script>
+        {{--<script>--}}
+            {{--var CityName=document.getElementById("defaultCityName").innerHTML = Utils.getCookie("CityName");--}}
+            {{--if (CityName == '北京市') {--}}
+                {{--document.getElementById("CustomService").innerHTML = '400-010-1515';--}}
+                {{--document.getElementById("CustomService").href = 'tel:400-010-1515';--}}
+            {{--}--}}
+            {{--else {--}}
+                {{--document.getElementById("CustomService").innerHTML = '400-066-8882';--}}
+                {{--document.getElementById("CustomService").href = 'tel:400-066-8882';--}}
+            {{--}--}}
+            {{--var ClientID = document.getElementById("ClientID").value;--}}
+            {{--if (ClientID == 'C100000175') {--}}
+                {{--document.getElementById("foot").style.display="none";--}}
+            {{--}--}}
+        {{--</script>--}}
 
 
         <script type="text/javascript">
