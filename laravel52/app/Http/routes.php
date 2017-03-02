c436ee68497dcf4610b90e23b1366b6fe961be92
王振涛 <755692172@qq.com>
2017-03-02 14:46:05 +0800

    //wang

--- laravel52/app/Http/routes.php
+++ laravel52/app/Http/routes.php
@@ -11,7 +11,23 @@ Route::get('/grab/{play_id?}','Home\GrabController@index');//选座页
 Route::any('/payGrab','Home\GrabController@payGrab');//购买座位
 Route::any('/checked','Home\GrabController@checked');//已购座位
 Route::any('/successUrl','Home\GrabController@successUrl');//支付成功同步地址
-
+/**
+ * 登陆
+ */
+Route::get('/login','Home\LoginController@login');//跳转登陆
+Route::post('/checklogin','Home\LoginController@checklogin');//登陆
+Route::get('/qqlogin','Home\LoginController@qqlogin');//qq登陆
+Route::get('/weibo','Home\LoginController@weibo');//微博登陆
+Route::get('/login_out','Home\LoginController@login_out');//退出登录
+/**
+ * 注册
+ */
+Route::get('/register','Home\LoginController@register');//跳转注册
+Route::post('/adds','Home\LoginController@adds');//注册
+Route::get('/nowapi_call','Home\LoginController@nowapi_call');//手机短信验证
+Route::get('/only','Home\LoginController@only');//注册唯一性
+Route::get('/short','Home\LoginController@short');//发送短信
+Route::get('/contrast','Home\LoginController@contrast');//验证码对比
 
 
 
